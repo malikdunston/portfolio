356bf2889adf7aca5c2a7ee825e8c94deaa5d495
Malik Dunston <hello@malikdunston.com>
2021-06-17 17:45:38 -0400

    form validation:

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,69 +4,70 @@ class Contact extends Component {
 		super(props);
 		this.state = {
 			php: "https://www.malikdunston.com/submit.php",
-			fields: [
-				{
+			fields: {
+				firstname: {
 					name: "firstname",
 					type: "text",
+					pattern: "^[a-zA-Z0-9_.+-]*(?:[a-zA-Z][a-zA-Z0-9_.+-]*){2,}$",
 					placeholder: "First",
 				},
-				{
+				lastname: {
 					name: "lastname",
 					type: "text",
+					pattern: "^[a-zA-Z0-9_.+-]*(?:[a-zA-Z][a-zA-Z0-9_.+-]*){2,}$",
 					placeholder: "Last",
 				},
-				{
+				phone: {
 					name: "phone",
 					type: "tel",
 					pattern: "^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$",
 					placeholder: "10-digit Phone",
 				},
-				{
+				email: {
 					name: "email",
 					type: "email",
 					pattern: "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
 					placeholder: "Email",
 				},
-				{
-					name: "resume",
-					type: "checkbox",
-					placeholder: "ex: I need a web app",
-					message: "Are you an employer?",
-					value: "1"
-				},
-				{
+				needs: {
 					name: "needs",
 					type: "textarea",
 					placeholder: "ex: I need a web app",
 					message: "Please describe your needs in detail."
 				}
-			]
+			},
+			formIsValid: false,
+			allErrors: []
 		}
 		this.submit = this.submit.bind(this)
+		this.handleBlur = this.handleBlur.bind(this)
+		this.handleChange = this.handleChange.bind(this)
 	}
 	submit(ev) {
 		ev.preventDefault();
-		let req = {
-			firstname: this.state.firstname,
-			lastname: this.state.lastname,
-			phone: this.state.phone,
-			email: this.state.email,
-			resume: parseInt(this.state.resume),
-			needs: this.state.needs
-		};
-		console.log(req); return
-		req = JSON.stringify(req)
-		fetch(this.state.php, {
-			method: 'POST',
-			body: req
-		}).then(checkError)
-		function checkError(response) {
-			if (response.status >= 200 && response.status <= 299) {
-				return response;
-			} else {
-				throw Error(response.statusText);
+		this.checkForm(this.state.allErrors, this.state.formIsValid);
+		if(this.state.formIsValid){
+			let req = {
+				firstname: this.state.firstname,
+				lastname: this.state.lastname,
+				phone: this.state.phone,
+				email: this.state.email,
+				needs: this.state.needs
+			};
+			console.log(req); return
+			req = JSON.stringify(req)
+			fetch(this.state.php, {
+				method: 'POST',
+				body: req
+			}).then(checkError)
+			function checkError(response) {
+				if (response.status >= 200 && response.status <= 299) {
+					return response;
+				} else {
+					throw Error(response.statusText);
+				}
 			}
-		}
+		} else alert("You have errors.")
 	}
 	componentDidMount() {
 		let fn;
@@ -75,14 +76,33 @@ class Contact extends Component {
 			firstname: fn
 		})
 		this.props.lightModeOnOff(true)()
+
+	}
+	validateField(validity, field){
+		if(!validity.valid){
+			field.error = true
+		} else field.error = null
+		this.setState({})
+	}
+	checkForm(e, formValid){
+		e = [];
+		Object.keys(this.state.fields).forEach(field=>{
+			if(this.state.fields[field].error){
+				e.push(this.state.fields[field])
+				this.setState({});
+			}
+		})
+		if(e.length === 0){formValid = true}
+		console.log(e, formValid);
 	}
 	handleBlur(ev, f){
-		console.dir(ev.target.validity.valid)
+		this.validateField(ev.target.validity, f);
+		f.blur = true;
+		this.setState({});
 	}
 	handleChange(ev, f){
-		let x = this.state.fields.filter(field=>field.name === f.name)[f.name]
-		console.log(x)
-		console.dir(ev.target.validity.valid)
+		this.validateField(ev.target.validity, f);
+		this.checkForm()
 	}
 	render() {
 		const input = (f) => (
@@ -97,39 +117,33 @@ class Contact extends Component {
 				placeholder={f.placeholder} />
 		)
 		const textarea = (f) => (
-			<textarea 
-				required
-				id={f.name}
-				maxlength="500"
+			<textarea required
+				id={f.name} 
+				maxLength="500"
 				name={f.name}
 				onBlur={ev=>{this.handleBlur(ev, f)}}
 				onChange={ev => { this.handleChange(ev, f) }}
 				placeholder={f.placeholder} />
 		)
 		return <div id="contact">
-			<header>
-				{this.state.firstname ? <h1>Hello, {this.state.firstname}.</h1> : ""}
-			</header>
-			<form action="#">
+			<form action="#"
+				className={!this.state.formIsValid ? "invalid-form" : ""}>
+				<header>{this.state.firstname ? <h1>Hello, {this.state.firstname}.</h1> : ""}</header>
 				<fieldset>
-					{this.state.fields.map(f=>{
-						return <label htmlFor={"#" + f.name}>
+					{Object.keys(this.state.fields).map(fieldName=>{
+						let f = this.state.fields[fieldName]
+						return <label 
+							key={f.name} 
+							htmlFor={"#" + f.name} 
+							className={(f.blur && f.error) ? "error" : ""}>
 							{f.message}
 							{f.type === "textarea" ? textarea(f) : input(f)}
 						</label>
 					})}
-					<label htmlFor="male">Male
-						<input type="radio" id="male" name="gender" value="male" />
-					</label>
-					<label htmlFor="female">Female
-						<input type="radio" id="female" name="gender" value="female" />
-					</label>
-					<label htmlFor="other">Other
-						<input type="radio" id="other" name="gender" value="other" />
-					</label>
-
 				</fieldset>
-				<input onClick={this.submit} type="submit" />
+				<input type="submit" 
+					onClick={this.submit}
+					className={"button" + (!this.state.formIsValid ? " inactive" : "")}/>
 			</form>
 		</div>
 	};
