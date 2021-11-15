50fc3bcedbe4b24a2ca280d0bb3b97c70d457262
Malik Dunston <hello@malikdunston.com>
2021-11-15 10:16:34 -0500

    src/Components/SliderJS moved to git/simple_slider master

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import getData from "./../Services/getData";
-import Slider from "./../Components/SliderJS/Slider";
+import { Slider } from '../Components/Requirements';
+import { Link } from 'react-router-dom';
 function Home( props ) {
 	useEffect(() => { props.getProjects() }, []);
+	const template = card => <div style={{position: "relative", width: "100%", height: "100%"}}>
+		{card.acf.cover ? <img src={card.acf.cover} 
+			alt={card.title.rendered}
+			style={{ objectFit:"cover", width:"100%", height:"100%", position:"absolute" }} /> : ""}
+		<div className="card-content"
+			style={{
+				bottom:"0",
+				width:"100%",
+				position:"absolute",
+				background: "rgba(0, 0, 0, .5)",
+				color: "white"
+			}}>
+			<h1 dangerouslySetInnerHTML={{__html: card.title.rendered}}></h1>
+			<Link to={"/work/"+card.slug} className={"button"}> View </Link>
+		</div>
+	</div>
 	return <div>
-		{props.projects ? <Slider cards={props.projects}
-			transition={200}
-			controls={true} /> : ""}
+		{props.projects ? <Slider cards={props.projects.filter(p => p.parent === 0)}
+			cardData={template}
+			controls={true}
+			cardSize={66.66}/> : ""}
 	</div>
 } 
 export default Home;
