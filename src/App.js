9eef51f71e108ce99519d10e7bd5303d3dcc617d
Malik Dunston <hello@malikdunston.com>
2021-11-18 22:49:18 -0500

    slider, UI decision

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ import Home from "./Pages/Home";
 function App( props ) {
 	const [ projects, setProjects ] = useState([]);
 	const getProjects = async () => {
-		let projects = await props.getData("graphic-design");
-		setProjects( projects );
+		let allProjs = await props.getData("graphic-design");
+		let parentProjs = allProjs.filter(proj => proj.parent === 0);
+		allProjs = parentProjs.map(proj => {
+			if(proj.parent === 0){
+				proj.projChildren = allProjs.filter(p => p.parent === proj.id);
+				console.log("projchildren", proj.projChildren);
+				return proj;
+			}
+		})
+		setProjects( allProjs );
 	}
 	return <div className={"App " + (props.location.pathname.split("/")[1] || "home")}>
 		{/* <Navigation /> */}
-		<Route exact path="/" 
-			render={ props => <Home { ...props } 
-				getProjects={getProjects} 
-				projects={projects} />}/>
+		<Route exact path="/" render={ props => <Home { ...props } 
+			getProjects={getProjects} 
+			projects={projects} />}/>
 	</div>
 }
 export default withRouter( getData( App ) );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,31 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import getData from "./../Services/getData";
-// npm install "C:\Users\crdun\Desktop\malik\work\web\simple_slider.git"
-import { Slider } from 'slides-cards';
 import { Link } from 'react-router-dom';
-function Home( props ) {
-	useEffect(() => { props.getProjects() }, []);
+import { Slider } from 'malikdunston-slider'
+function Home({ projects, getProjects }) {
+	useEffect(() => { getProjects() }, []);
 	const template = card => <div style={{position: "relative", width: "100%", height: "100%"}}>
 		{card.acf.cover ? <img src={card.acf.cover} 
 			alt={card.title.rendered}
 			style={{ objectFit:"cover", width:"100%", height:"100%", position:"absolute" }} /> : ""}
-		<div className="card-content"
-			style={{
-				bottom:"0",
-				width:"100%",
-				position:"absolute",
-				background: "rgba(0, 0, 0, .5)",
-				color: "white"
-			}}>
+		<div className="card-content" style={{
+			bottom:"0",
+			width:"100%",
+			position:"absolute",
+			background: "rgba(0, 0, 0, .5)",
+			color: "white"
+		}}>
 			<h1 dangerouslySetInnerHTML={{__html: card.title.rendered}}></h1>
 			<Link to={"/work/"+card.slug} className={"button"}> View </Link>
 		</div>
 	</div>
 	return <div>
-		{props.projects ? <Slider cards={props.projects.filter(p => p.parent === 0)}
+		{/* {projects ? <Slider cards={projects.filter(p => p.parent === 0)}
 			cardData={template}
 			controls={true}
-			cardSize={66.66}/> : ""}
+			cardSize={66.66}/> : ""} */}
+		<Slider cards={projects}
+			template={template}
+			controls={true}
+			size={100}/>
 	</div>
 } 
 export default Home;
