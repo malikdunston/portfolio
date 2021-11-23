b50d0ca63e5ac0687e6c048d07e0ee9a43c31148
Malik Dunston <hello@malikdunston.com>
2021-11-23 14:07:26 -0500

    CaseStudy

--- src/Pages/CaseStudy.js
+++ src/Pages/CaseStudy.js
@@ -5,11 +5,14 @@ function CaseStudy({ projects, thisProject, match, selectProject}) {
 			slug: match.params.projSlug
 		})
 	}, [ projects ]);
-	return thisProject ? <div className={"casestudy " + thisProject.slug}>
+	return thisProject ? <div id={"casestudy"} className={"casestudy " + thisProject.slug}>
 		<header>
 			<img src={thisProject.acf.cover} alt={thisProject.title.rendered} />
 			<h1>{thisProject.title.rendered}</h1>
 		</header>
+		{thisProject.projChildren.map((p, i) => <article>
+			{p.title.rendered}
+		</article>)}
 	</div> : ""
 } 
 export default CaseStudy;
