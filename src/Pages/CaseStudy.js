dd667e6d5c1a82b89f719b942cdc419eaabbe34c
Malik Dunston <hello@malikdunston.com>
2021-11-24 01:09:57 -0500

    case-study.css

--- src/Pages/CaseStudy.js
+++ src/Pages/CaseStudy.js
@@ -6,19 +6,21 @@ function CaseStudy({ projects, thisProject, match, selectProject}) {
 			slug: match.params.projSlug
 		})
 	}, [ projects ]);
-	return thisProject ? <div className={"casestudy " + thisProject.slug}>
+	return thisProject ? <div className={"case-study " + thisProject.slug}>
 		<header>
 			<img src={thisProject.acf.cover} alt={thisProject.title.rendered} />
-			<h1>{thisProject.title.rendered}</h1>
+			<div className="shader"></div>
+			<h1 dangerouslySetInnerHTML={{ __html: thisProject.title.rendered }}></h1>
 		</header>
 		{thisProject.projChildren.map((p, i) => <article>
-			<section>
-				<h3>{p.title.rendered}</h3>
-				<p>Some content goes here......</p>
+			<section className={"text " + organizeContent( p.content.rendered ).length > 0 ? "" : "flex"}>
+				<div className="column">
+					<h3 dangerouslySetInnerHTML={{ __html: p.title.rendered }}></h3>
+					<p>Some content goes here......</p>
+				</div>
 			</section>
 			<section className="content">
-				{organizeContent( p.content.rendered ).map((img, i) => <div className="figure"
-					key={img.src}>
+				{organizeContent( p.content.rendered ).map((img, i) => <div className="figure" key={img.src}>
 					<img src={img.src} alt="" />
 					{img.caption ? <p>{img.caption}</p> : ""}
 				</div>)}
