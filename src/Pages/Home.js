0444d269384f348f4278f11a9d1e769d467f226c
Malik Dunston <hello@malikdunston.com>
2021-12-08 11:20:02 -0500

    bugs fixed from master

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,34 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Slider } from 'malikdunston-slider';
-import getBreakpoints from '../Services/getBreakpoints';
 function Home({ projects, breakpoint }) {
-	const template = card => <div style={{width: "100%", height: "100%"}}>
-		{/* for projects that don't yet have two covers.... */}
-		{card.acf.cover ? <img src={card.acf.cover} 
-			alt={card.title.rendered}
-			style={{ objectFit:"cover", width:"100%", height:"100%", position:"absolute" }} /> : ""}
-		{card.acf.cover2 ? <img src={card.acf.cover2} 
-			alt={card.title.rendered}
+	const template = proj => <div style={{width: "100%", height: "100%"}}>
+		{proj.acf.cover2 ? <img src={proj.acf.cover2} 
+			alt={proj.title.rendered}
 			style={{ objectFit:"cover", width:"100%", height:"100%", position:"absolute" }} /> : ""}
 		<div className="shader"></div>
 		<div className="card-content">
-			<h1 dangerouslySetInnerHTML={{__html: card.title.rendered}}></h1>
-			<p>{card.acf.about}</p>
-			{/* { breakpoint.size <= 1000 ? <Link to={"/work/"+card.slug}>Explore</Link> : ""} */}
+			<h1 dangerouslySetInnerHTML={{__html: proj.title.rendered}}></h1>
+			<p>{proj.acf.about}</p>
 		</div>
 	</div>
 	const breadcrumbs = proj => {
 		let img = <img src={proj.acf.cover} alt=""/>;
-		return proj.slug === "about" && breakpoint.size >= 1000 ? img : <Link to={"/work/"+proj.slug}>{img}</Link>
+		return <Link to={"/work/"+proj.slug}>{img}</Link>
 	}
 	return <div>
-		<Slider cards={projects}
+		{projects ? <Slider cards={projects}
 			axis={"X"}
 			template={template}
 			mouseEffect={true}
 			breadcrumbs={breadcrumbs}
-			size={100}/>
+			size={100}/> : ""}
 	</div>
 } 
 export default Home;
