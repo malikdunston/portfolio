1ae3f574c91ab042f184f77d3ba74269de0b8aff
Malik Dunston <hello@malikdunston.com>
2022-04-26 19:58:39 -0400

    print resume -> resume.js html

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -10,7 +10,64 @@ function Resume( props ){
 		getResume();
 	}, []);
 	return <div id={"resume"}>
-		Malik Dunston
+		<section id="about">
+			<h1>Malik Dunston</h1>
+			<p>Atlanta, GA - 770.895.2061</p>
+			<p><a href="https://www.malikdunston.com">malikdunston.com</a></p>
+			<p><a href="mailto:malik.dunston.1024@gmail.com">malik.dunston.1024@gmail.com</a></p>
+		</section>
+		<section id="skills">
+			<h2>Skills</h2>
+			<p>
+				<div>React<span>, </span></div>
+				<div>Typescript<span>, </span></div>
+			</p>
+		</section>
+		<section id="experience">
+			<h2>Experience</h2>
+			<div>
+				<div>
+					<h3>
+						<div>Employer<span>, Location</span></div>
+						<div>November 2019 – May 2021</div>
+					</h3>
+					<p>Employer Description</p>
+				</div>
+				<ul>
+					<h4>Role</h4>
+					<li>Articulated XYZ to do ABC</li>
+					<li>Developed XYZ to do ABC</li>
+				</ul>
+			</div>
+		</section>
+		<section id="education">
+			<h2>Education</h2>
+			<div>
+				<div>
+					<h3>
+						<div>Bachelor of Science</div>
+						<div>, Major</div>
+					</h3>
+					<p>
+						<span>Univeristy</span>
+						<span>, Location</span>
+						<span>, August 2019</span>
+					</p>
+				</div>
+				<ul>
+					<li>Dean's List 2017</li>
+					<li>Code Club Member</li>
+				</ul>
+			</div>
+		</section>
+		<section id="certifications">
+			<h2>Certifications</h2>
+			<div>
+				<h3>Cert Title</h3>
+				<p>Issuer<span>, August 2019</span></p>
+			</div>
+
+		</section>
 	</div>
 } 
 export default getData( Resume );
