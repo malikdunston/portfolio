917cd5cd9369117a2abe9d570c9373284e8843b2
Malik Dunston <hello@malikdunston.com>
2022-04-27 02:55:06 -0400

    Gsheets api, Resume State, getData -> Port and Res

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,73 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import getData from "./../Services/getData";
 function Resume( props ){
-	const [ resume, setResume ] = useState([]);
-	const getResume = async () => {
-		let data = await props.getData("resume");
-		setResume(data);
+	const [ resume, setResume ] = useState({
+		experience: [],
+		education: [],
+		skills: [],
+	});
+	const getResumeData = async () => {
+		let experience = await props.getResume("experience");
+		let education = await props.getResume("education");
+		let skills = await props.getResume("skills");
+		setResume(oldResume => ({
+			experience: experience,
+			education: education,
+			skills: skills
+		}));
+	}
+	const printResume = () => {
+		window.print()
 	}
 	useEffect(() => { 
-		getResume();
+		getResumeData();
 	}, []);
-	return <div id={"resume"}>
-		<section id="about">
-			<h1>Malik Dunston</h1>
-			<p>Atlanta, GA - 770.895.2061</p>
-			<p><a href="https://www.malikdunston.com">malikdunston.com</a></p>
-			<p><a href="mailto:malik.dunston.1024@gmail.com">malik.dunston.1024@gmail.com</a></p>
-		</section>
-		<section id="skills">
-			<h2>Skills</h2>
-			<p>
-				<div>React<span>, </span></div>
-				<div>Typescript<span>, </span></div>
-			</p>
-		</section>
-		<section id="experience">
-			<h2>Experience</h2>
-			<div>
+	return <div>
+		<div id="resume_menu">
+			<button onClick={printResume}>Print</button>
+		</div>
+		<div id="resume">
+			<section id="about">
+				<h1>Malik Dunston</h1>
+				<p>Atlanta, GA - 770.895.2061</p>
+				<p><a href="https://www.malikdunston.com">malikdunston.com</a></p>
+				<p><a href="mailto:malik.dunston.1024@gmail.com">malik.dunston.1024@gmail.com</a></p>
+			</section>
+			<section id="skills">
+				<h2>Skills</h2>
+				<p>
+					<span>React<span>, </span></span>
+					<span>Typescript<span>, </span></span>
+				</p>
+			</section>
+			<section id="experience">
+				<h2>Experience</h2>
 				<div>
 					<h3>
 						<div>Employer<span>, Location</span></div>
 						<div>November 2019 – May 2021</div>
 					</h3>
 					<p>Employer Description</p>
+					<ul>
+						<h4>Role</h4>
+						<li>Articulated XYZ to do ABC</li>
+						<li>Developed XYZ to do ABC</li>
+					</ul>
 				</div>
-				<ul>
-					<h4>Role</h4>
-					<li>Articulated XYZ to do ABC</li>
-					<li>Developed XYZ to do ABC</li>
-				</ul>
-			</div>
-		</section>
-		<section id="education">
-			<h2>Education</h2>
-			<div>
+			</section>
+			<section id="education">
+				<h2>Education</h2>
 				<div>
 					<h3>
-						<div>Bachelor of Science</div>
-						<div>, Major</div>
+						<div>Bachelor of Science<span>, Major</span></div>
+						<div>Univeristy<span>, Location</span><span>, August 2019</span></div>
 					</h3>
-					<p>
-						<span>Univeristy</span>
-						<span>, Location</span>
-						<span>, August 2019</span>
-					</p>
+					<ul>
+						<li>Dean's List 2017</li>
+						<li>Code Club Member</li>
+					</ul>
 				</div>
-				<ul>
-					<li>Dean's List 2017</li>
-					<li>Code Club Member</li>
-				</ul>
-			</div>
-		</section>
-		<section id="certifications">
-			<h2>Certifications</h2>
-			<div>
-				<h3>Cert Title</h3>
-				<p>Issuer<span>, August 2019</span></p>
-			</div>
-
-		</section>
+			</section>
+			<section id="certifications">
+				<h2>Certifications</h2>
+				<div>
+					<h3>Cert Title</h3>
+					<p>Issuer<span>, August 2019</span></p>
+				</div>
+			</section>
+		</div>
 	</div>
 } 
 export default getData( Resume );
--- src/Services/getData.js
+++ src/Services/getData.js
@@ -1,21 +1,17 @@
 import React from 'react';
 const { REACT_APP_WP_URL, REACT_APP_RESUME } = process.env;
-export default function getData (OrigComp) {
-	return function GetDataHOC ( props ) {
-		const getData = async ( lookFor, params ) =>{
-			params = params || {};
-			let api;
-			if(lookFor == "resume"){
-				api = REACT_APP_RESUME;
-				params.action = "getUsers";
-			}else{
-				params["per_page"] = params.perPage || "100";
-				api = `https://${REACT_APP_WP_URL}/wp-json/wp/v2/${lookFor}`;
-			}
-			let url = new URL(api);			
+export default function getData(OrigComp) { // HOC
+	return function GetDataHOC(props) { // Data
+		const getPortfolio = async (lookFor, params = { ["per_page"]: "100" }) => {
+			let url = new URL(`https://${REACT_APP_WP_URL}/wp-json/wp/v2/${lookFor}`);
 			url.search = new URLSearchParams(params).toString();
-			return  await fetch(url).then(resp => resp.json());
+			return await fetch(url).then(resp => resp.json())
 		}
-		return <OrigComp {...props} getData={getData} />
+		const getResume = async (table = "experience") => {
+			let url = new URL(REACT_APP_RESUME);
+			url.search = new URLSearchParams({table: table}).toString();
+			return await fetch(url).then(res => res.json())
+		}
+		return <OrigComp {...props} getData={getPortfolio} getResume={getResume} />
 	}
 }
